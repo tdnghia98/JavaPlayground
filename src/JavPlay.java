6b8eacfc46625cf39aeb7c626d65857c0871e596
tdnghia98 <tdnghia98@gmail.com>
2018-03-14 21:41:45 +0100

    kjbk

--- src/JavPlay.java
+++ src/JavPlay.java
@@ -1,5 +1,12 @@
-public class JavPlay {
+import javax.swing.*;
+
+public class JavPlay extends JFrame{
+    private JPanel panM;    // Le terrain
+
     public JavPlay(){
+        // Dessiner le plan du terrain
 
     }
+
+
 }
