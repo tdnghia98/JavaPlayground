9b4c2a786dd299f04bd194085a01c8f1a25e4f2c
Louis U <ung.louis@hotmail.fr>
2018-03-15 09:14:10 +0100

    kjbk

--- src/PanelPlay.java
+++ src/PanelPlay.java
@@ -7,8 +7,9 @@ public class PanelPlay extends JFrame implements ActionListener {
     public PanelPlay(){
 
         play = new JButton("Play");
+        play.setBounds(120,5,60,40);
         this.add(play);
-        play.addActionListener(e -> "Exécuter les méthodes");
+        play.addActionListener(e -> System.out.println("Exécuter les méthodes"));
 
 
     }
