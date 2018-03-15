94b53931db2f5e09c18f1cfb3f22a5d4daa3284e
tdnghia98 <tdnghia98@gmail.com>
2018-03-15 09:25:34 +0100

    kjbk

--- src/PanelPlay.java
+++ src/PanelPlay.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class PanelPlay extends JFrame implements ActionListener {
+public class PanelPlay extends JPanel {
     private JButton play;
+    ArrayList<Integer> commandes;
 
-    public PanelPlay(){
-
+    public PanelPlay(PanelButton butts){
+        // Recevoir le tableau de commande
+        commandes = butts.commande;
+        // Creer le bouton "Play"
         play = new JButton("Play");
         play.setBounds(120,5,60,40);
         this.add(play);
