10821f707f2b8dae87869a06d34589055ab8d81c
Cam <camille.bossard@insa-lyon.fr>
2018-03-29 09:58:33 +0200

    boussole

--- src/PanelUser.java
+++ src/PanelUser.java
@@ -17,6 +17,30 @@ public class PanelUser extends JPanel {
         uScroll = new JScrollPane(uText);
         uScroll.setBounds(5,5,200,300);
         add(uScroll);
+
+        imageBNord = new JLabel();
+        imageBEst = new JLabel();
+        imageBSud = new JLabel();
+        imageBOuest = new JLabel();
+        add(imageBNord);
+        add(imageBEst);
+        add(imageBSud);
+        add(imageBOuest);
+        setLayout(null);
+
+        if (p.getDir() == nord) { // boussole nord
+            imageBNORD.setIcon(new ImageIcon("./graph/BOUSSOLENORD.png"));
+            imageBNORD.setBounds();
+        } else if (p.getDir() == sud) { // boussole sud
+            imageBSUD.setIcon(new ImageIcon("./graph/BOUSSOLESUD.png"));
+            imageBSUD.setBounds();
+        } else if (p.getDir() == est) { // boussole est
+            imageBEST.setIcon(new ImageIcon("./graph/BOUSSOLEEST.png"));
+            imageBEST.setBounds();
+        } else if (p.getDir() == ouest) { // boussole ouest
+            imageBOUEST.setIcon(new ImageIcon("./graph/BOUSSOLEOUEST.png"));
+            imageBOUEST.setBounds();
+        }
     }
 
     public void paintComponent(Graphics g) {
