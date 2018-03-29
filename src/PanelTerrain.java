30e8aafacbdbd4fc21ecad78d6921fd7c73aec61
tdnghia98 <tdnghia98@gmail.com>
2018-03-29 09:41:08 +0200

    Reorganize panelterrain

--- src/PanelTerrain.java
+++ src/PanelTerrain.java
@@ -7,25 +7,46 @@ public class PanelTerrain extends JPanel {
     Terrain t;
     private int[][] map;
     Personnage person;
+    JLabel imageCaseFinale;
+    JLabel imageDiamant;
+    JLabel imageCaillou;
+    JLabel imageNord;
+    JLabel imageEst;
+    JLabel imageSud;
+    JLabel imageOuest;
+
 
 
     public PanelTerrain(Terrain terre, Personnage p) {
         person = p;
         t = terre;
         map = terre.map;
+        // Creer et ajouter tous les labels et images
+        imageCaseFinale = new JLabel();
+        imageDiamant = new JLabel();
+        imageCaillou = new JLabel();
+        imageNord = new JLabel();
+        imageEst = new JLabel();
+        imageSud = new JLabel();
+        imageOuest = new JLabel();
+        add(imageCaseFinale);
+        add(imageDiamant);
+        add(imageCaillou);
+        add(imageNord);
+        add(imageEst);
+        add(imageOuest);
         setLayout(null);
     }
 
     public void paintComponent (Graphics g) {
         // Dessiner le plan du terrain
         int carre_cote = 50;    // La taille d'une case
-        g.setColor(Color.green);
         for (int i  = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
 //                System.out.println("i = " + i + " , j = " +j);
                 // On dessine d'abord toutes les cases
-
                 Graphics2D g2 = (Graphics2D) g;
+                // Dessiner la bordure
                 g2.setStroke(new BasicStroke(5));
                 g2.setColor(Color.black);
                 g2.drawRect(j * carre_cote, i * carre_cote, carre_cote, carre_cote);
@@ -35,49 +56,36 @@ public class PanelTerrain extends JPanel {
                 // On dessine ensuite des objets
                 switch (map[i][j]) {    // Taille d'un carre: 50x50 px
                     case 1:  // La case finale
-                        JLabel imageCaseFinale = new JLabel();
                         imageCaseFinale.setIcon(new ImageIcon("./graph/Case finale.png"));
                         imageCaseFinale.setBounds(j*50,i*50,50,50);
-                        add(imageCaseFinale);
                         break;
                     case 2:
                         //g.setColor(Color.blue); // Le diamant
-                        JLabel imageDiamant = new JLabel();
                         imageDiamant.setIcon(new ImageIcon("./graph/diamant.png"));
                         imageDiamant.setBounds(j*50,i*50,50,50);
-                        add(imageDiamant);
                         break;
                     case 3:  // Le caillou
-                        JLabel imageCaillou = new JLabel();
                         imageCaillou.setIcon(new ImageIcon("./graph/Caillou 1.png"));
                         imageCaillou.setBounds(j*50,i*50,50,50);
-                        add(imageCaillou);
                         break;
                     case 4:  // Le personnage
                         switch (person.dir) {
                             case 0: //nord
-                                JLabel imageNord = new JLabel();
                                 imageNord.setIcon(new ImageIcon("./graph/Bon homme nord.png"));
                                 imageNord.setBounds(j*50,i*50,50,50);
-                                add(imageNord);
                                 break;
                             case 1: //est
-                                JLabel imageEst = new JLabel();
                                 imageEst.setIcon(new ImageIcon("./graph/Bon homme est.png"));
                                 imageEst.setBounds(j*50,i*50,50,50);
-                                add(imageEst);
                                 break;
                             case 2: //sud
-                                JLabel imageSud = new JLabel();
                                 imageSud.setIcon(new ImageIcon("./graph/Bon homme sud.png"));
                                 imageSud.setBounds(j*50,i*50,50,50);
                                 add(imageSud);
                                 break;
                             case 3: //ouest
-                                JLabel imageOuest = new JLabel();
                                 imageOuest.setIcon(new ImageIcon("./graph/Bon homme ouest.png"));
                                 imageOuest.setBounds(j*50,i*50,50,50);
-                                add(imageOuest);
                                 break;
 
                         }
