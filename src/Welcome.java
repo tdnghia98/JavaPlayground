35930e3bcef1c7b4e982b461a5b651c69c03f39c
dmvan <mai-van.dinh@insa-lyon.fr>
2018-04-04 19:52:18 +0200

    Change appearance "Welcome"

--- src/Welcome.java
+++ src/Welcome.java
@@ -15,21 +15,35 @@ public class Welcome extends JFrame {
         setLocationRelativeTo(null);
 
         // Panel des choix
+        Font police = new Font ("Arial", Font.BOLD, 30);
         choices = new JPanel();
         choices.setLayout(null);
         choices.setBounds(150, 150, 200, 200);
         JButton play = new JButton("Play");
-        play.setBounds(0, 0, 100, 100);
+        play.setBounds(0, 0, 200, 100);
+        play.setFont(police);
+        play.setBackground(Color.yellow);
         JButton quit = new JButton("Quit");
-        quit.setBounds(0, 110, 100, 100);
+        quit.setBounds(0, 100, 200, 100);
+        quit.setFont(police);
+        quit.setBackground(Color.yellow);
         choices.add(play);
         choices.add(quit);
         // Main panel
         mainPan = new JPanel();
         mainPan.setLayout(null);
         mainPan.setBounds(0, 0, this.getWidth(), this.getHeight());
-        mainPan.setBackground(Color.green);
-
+        mainPan.setBackground(Color.cyan);
+        JLabel bienvenue = new JLabel ("Bienvenue à Java Playground");
+        bienvenue.setBounds (30,50,500,50);
+        bienvenue.setFont(police);
+        mainPan.add(bienvenue);
+        JLabel authors = new JLabel("BOSSARD Camille, DINH Mai Van, TRAN Duy Nghia, UNG Louis");
+        authors.setBounds(130,370,500,50);
+        mainPan.add(authors);
+        JLabel authors2 = new JLabel("Groupe 97 ASINSA 2");
+        authors2.setBounds(340,390,200,50);
+        mainPan.add(authors2);
         add(mainPan);
         mainPan.add(choices);
 
